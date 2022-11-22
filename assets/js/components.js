6677f9a21ff30b97285088e3dd2522594d4aec25
Ali Servet Donmez <asd@pittle.org>
2022-11-22 21:41:45 +0100

    Hamburger menu as proper JSX

--- assets/js/components.js
+++ assets/js/components.js
@@ -74,14 +74,14 @@ class HomePage extends React.Component {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
+                className="h-10 w-10"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </button>
